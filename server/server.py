40a6e7b16330b86b188b7e2b9f0d2084bb872f18
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2016-12-16 22:20:57 -0500

    convert project to maven

--- server/server.py
+++ server/server.py
@@ -1,1 +1,39 @@
-import tornado
+import tornado.ioloop
+import tornado.web
+
+from tornado.options import define, options, parse_command_line
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        self.write("This is your response\n")
+        self.finish()
+
+class WebSocketHandler(tornado.web.WebSocketHandler):
+
+    def __init__(self):
+        self.ws_clients = []
+
+    def open(self):
+        if(self not in self.ws_clients)
+        self.ws_clients.append(self)
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        for client in ws_clients:
+            client.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+app = tornado.web.Application([
+    (r'/', IndexHandler),
+])
+
+if __name__ == '__main__':
+    parse_command_line()
+    app.listen(8886)
+    tornado.ioloop.IOLoop.instance().start()
